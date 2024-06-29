4b85f79afd5cfd46761fba4c0a4b01b24bc54a44
bajajsanchit <65711927+bajajsanchit@users.noreply.github.com>
2024-06-29 14:13:00 +0530

    siteplan modal

--- src/components/ProjectPlans/index.js
+++ src/components/ProjectPlans/index.js
@@ -3,6 +3,7 @@ import "./styles.scss";
 
 const ProjectPlans = () => {
 	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [clickedCard, setClickedCard] = useState(null);
 
 	const cards = [
 		{ id: 1, imgSrc: "layout-1.png", text: "Site Layout" },
@@ -23,8 +24,15 @@ const ProjectPlans = () => {
 		},
 	];
 
-	const openModal = () => setModalIsOpen(true);
-	const closeModal = () => setModalIsOpen(false);
+	const openModal = (card) => {
+		setClickedCard(card);
+		setModalIsOpen(true);
+	};
+
+	const closeModal = () => {
+		setModalIsOpen(false);
+		setClickedCard(null);
+	};
 
 	return (
 		<div>
@@ -34,7 +42,8 @@ const ProjectPlans = () => {
 						key={card.id}
 						imgSrc={card.imgSrc}
 						text={card.text}
-						onClick={openModal}
+						onClick={() => openModal(card)}
+						className={card.id === 1 ? "card special" : "card"}
 					/>
 				))}
 			</div>
@@ -42,63 +51,73 @@ const ProjectPlans = () => {
 			{modalIsOpen && (
 				<>
 					<div className="overlay-plan" onClick={closeModal} />
-					<Modal closeModal={closeModal} />
+					<Modal clickedCard={clickedCard} closeModal={closeModal} />
 				</>
 			)}
 		</div>
 	);
 };
 
-const Card = ({ imgSrc, text, onClick }) => (
-	<div className="card" onClick={onClick}>
+const Card = ({ imgSrc, text, onClick, className }) => (
+	<div className={className} onClick={onClick}>
 		<img src={imgSrc} alt="Card" className="card-image" />
 		<p>{text}</p>
 	</div>
 );
 
-const Modal = ({ closeModal }) => (
-	<div className="modal">
-		<h2>Contact Us and Book your Dream Home Today @ 10 Lacs</h2>
-		<form
-			action="https://formspree.io/f/{your_form_id}"
-			method="POST"
-			className="contact-form"
-		>
-			<div className="form-group">
-				<label htmlFor="name" className="form-label">
-					Name:
-				</label>
-				<input
-					type="text"
-					id="name"
-					name="name"
-					required
-					className="form-input"
-				/>
+const Modal = ({ clickedCard, closeModal }) => (
+	<div className={`modal ${clickedCard?.id === 1 ? "special" : ""}`}>
+		{clickedCard?.id === 1 ? (
+			<img
+				src={clickedCard.imgSrc}
+				alt="Modal Content"
+				className="modal-image"
+			/>
+		) : (
+			<div>
+				<h2>Contact Us and Book your Dream Home Today @ 10 Lacs</h2>
+				<form
+					action="https://formspree.io/f/{your_form_id}"
+					method="POST"
+					className="contact-form"
+				>
+					<div className="form-group">
+						<label htmlFor="name" className="form-label">
+							Name:
+						</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							required
+							className="form-input"
+						/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="contact" className="form-label">
+							Contact:
+						</label>
+						<input
+							type="text"
+							id="contact"
+							name="contact"
+							required
+							className="form-input"
+						/>
+					</div>
+					<button type="submit" className="form-button">
+						Send
+					</button>
+					<button
+						type="button"
+						onClick={closeModal}
+						className="form-button close-button"
+					>
+						Close
+					</button>
+				</form>
 			</div>
-			<div className="form-group">
-				<label htmlFor="contact" className="form-label">
-					Contact:
-				</label>
-				<input
-					type="text"
-					id="contact"
-					name="contact"
-					required
-					className="form-input"
-				/>
-			</div>
-			<button type="submit" className="form-button">
-				Send
-			</button>
-			<button
-				type="button"
-				onClick={closeModal}
-				className="form-button close-button"
-			>
-				Close
-			</button>
-		</form>
+		)}
 	</div>
 );
 
